ac36043b4739543b84ca5fd606e014cdbd76b5c5
Gaetano Mondelli <gaetano.mondelli@gmail.com>
2024-06-08 21:15:07 +0100

    feat: create oracle example

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -2,9 +2,9 @@
 
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import * as xrpl from "xrpl";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
+import * as xrpl from "xrpl";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Address } from "~~/components/scaffold-eth";
 
@@ -12,6 +12,10 @@ const Home: NextPage = () => {
   const [balance, setBalance] = useState<string | null>(null);
   const [connectionStatus, setConnectionStatus] = useState("Initializing...");
   const [client, setClient] = useState<any>(null);
+  const [oracle, setOracle] = useState<any>(null);
+
+  const myseed = "sEdT8WPqpEAVpygKSrA6svMHYU8NbaG";
+  const sequence = 1347914;
 
   useEffect(() => {
     const connectToXRPL = async () => {
@@ -24,15 +28,16 @@ const Home: NextPage = () => {
         // Subscribe to an account
         // await client.request({
         //   command: "subscribe",
-        //   accounts: ["rEMuMyGj2wMm3iJbeX1EvhVZjjzffrEG8y"],
+        //   accounts: ["rLXbPN2kiiQ4skm7nU7Qe7ADNR8mqKLAyZ"],
         // });
 
         const response = await client.request({
           command: "account_info",
-          account: "rEMuMyGj2wMm3iJbeX1EvhVZjjzffrEG8y",
+          account: "rLXbPN2kiiQ4skm7nU7Qe7ADNR8mqKLAyZ",
           ledger_index: "validated",
         });
 
+        setClient(client);
         setBalance(response.result.account_data.Balance);
         console.log("Account balance:", response.result.account_data.Balance);
       } catch (error) {
@@ -58,7 +63,28 @@ const Home: NextPage = () => {
           <div>Connection Status: {connectionStatus}</div>
           {connectionStatus === "Connected" && <div>XRPL Client Connected - Balance {balance}</div>}
         </div>
+        <img src="https://s1.xrpl.to/token/0413ca7cfc258dfaf698c02fe304e607" className="h-20 w-20" alt="logo" />
+        <button
+          className="mt-4"
+          onClick={async () => {
+            if (!client) return;
+            const response = await client.request({
+              command: "CreateOracle",
+              account: "rLXbPN2kiiQ4skm7nU7Qe7ADNR8mqKLAyZ",
+              Symbol: "XRP",
+              SymbolClass: "63757272656E6379",
+              PriceUnit: "USD",
+              Provider: "70726F7669646572",
+              ledger_index: "validated",
+            });
+
+            console.log(response);
+          }}
+        >
+          Create oracle
+        </button>
       </div>
+      Oracle Response {oracle}
     </>
   );
 };
